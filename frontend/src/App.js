0dd6fc5701cb11bf7ef63eed4fbc3f9fbcafe58c
Niranjanluthra24 <77604350+Niranjanluthra24@users.noreply.github.com>
2024-04-11 09:30:54 +0530

    My project

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import { Navbar } from './Components/Navbar/Navbar';
+import {Shop} from "./Pages/Shop"
+import {ShopCategory} from "./Pages/ShopCategory";
+import { Product } from './Pages/Product';
+import { Cart } from './Pages/Cart';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginSignup } from './Pages/LoginSignup';
+function App() {
+  return (
+    <div >
+      <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/mens" element={<ShopCategory category="men" />} />
+        <Route path="/womens" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kid" />} />
+        <Route path='/product' element={<Product />}>
+          <Route path=':id' element={<Product />} />
+        </Route>
+        <Route path='/login' element={<LoginSignup/>}/>
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+     </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Popular } from '../Components/Popular/Popular'
+import {Hero} from "../Components/Hero/Hero"
+export const Shop = () => {
+  return (
+    <div>
+        <Hero />
+         <Popular /> 
+    </div>
+  )
+}
